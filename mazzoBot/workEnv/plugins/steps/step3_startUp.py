b060eafa98874b4d0ce25d50d92294fcb2229995
lorenzodamora <damora.lorenzo.studente@itispaleocapa.it>
2024-01-29 13:55:25 +0100

    aggiunto contatto in step 3

--- mazzoBot/workEnv/plugins/steps/step3_startUp.py
+++ mazzoBot/workEnv/plugins/steps/step3_startUp.py
@@ -4,12 +4,14 @@ questo file contiene il primo step informativo: il webinar
 step 3
 """
 from pyrogram.types import Message as Msg, InlineKeyboardMarkup as Ikm, InlineKeyboardButton as Ikb
+from asyncio import sleep
 from ..lang import lc
+from ..handler import mycontact
 
 
 async def step3(msg: Msg):
     """Manda il link video della startUp"""
-    await msg.reply(
+    msg = await msg.reply(
         text="Ovviamente per iniziare a vivere la vita da film che sogni!\n"
              "Il modo più veloce per entrambi è guardare questo video!\n/contatto" if await lc(msg.chat.id) else
         "Of course, to start living the movie-like life you dream of!\n"
@@ -22,3 +24,5 @@ async def step3(msg: Msg):
             ]]),
         disable_web_page_preview=True,  # Disabilita l'anteprima del sito web se presente
     )
+    await sleep(20)
+    await mycontact(msg)
